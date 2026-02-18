aae22b3c0c29e5500b511e1e1b82cce9b9348b67
ppathak-19 <priyapathak.work@gmail.com>
2026-02-18 13:17:44 +0530

    using backend apis in frontend

--- server/app.js
+++ server/app.js
@@ -1,12 +1,49 @@
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const mongoose = require("mongoose");
-const blogRoutes = require("./routes/blogRoutes");
+// const blogRoutes = require("./routes/blogRoutes");
 
 // express app
-const app = express();
+// const app = express();
 
 // connect to mongodb & listen for requests
+// const dbURI =
+//   "mongodb+srv://priya-19:123456priya@cluster0.nh0urni.mongodb.net/?appName=Cluster0";
+
+// mongoose
+//   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log(err));
+
+// ----register view engine----
+// app.set("view engine", "ejs");
+
+// middleware & static files
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(morgan("dev"));
+
+// // routes
+// app.get("/", (req, res) => {
+//   res.redirect("/blogs");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.render("about", { title: "About" });
+// });
+
+// // blog routes
+// app.use("/blogs", blogRoutes);
+
+// // 404 page
+// app.use((req, res) => {
+//   res.status(404).render("404", { title: "404" });
+// });
+
+// -----Full stack with react api routes-----
+const app = express();
+
 const dbURI =
   "mongodb+srv://priya-19:123456priya@cluster0.nh0urni.mongodb.net/?appName=Cluster0";
 
@@ -15,31 +52,19 @@ mongoose
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-// register view engine
-app.set("view engine", "ejs");
-
-// middleware & static files
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-// routes
-app.get("/", (req, res) => {
-  res.redirect("/blogs");
-});
+// If a request contains JSON in its body, read it and convert it into a JavaScript object.
+app.use(express.json());
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from backend 👋" });
 });
-
-// blog routes
-app.use("/blogs", blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+  res.status(404).json({ error: "Not found" });
 });
