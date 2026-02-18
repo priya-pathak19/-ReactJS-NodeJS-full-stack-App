26e7ec1893a55adcf7d2ba0795e72d7027b773e4
ppathak-19 <priyapathak.work@gmail.com>
2026-02-18 19:57:13 +0530

    added worker comments

--- server/temporal/worker.ts
+++ server/temporal/worker.ts
@@ -1,23 +1,42 @@
+// This file starts a Temporal Worker process.
+// A worker is a long-running background service that:
+// polls Temporal Server
+// executes workflows
+// executes activities
+// Without this running, no workflow will ever execute, even if your API starts them.
 import { NativeConnection, Worker } from "@temporalio/worker";
+// Worker → creates the worker process
+// NativeConnection → low-level gRPC (binary messages using Protocol Buffers) connection to Temporal Server -> gRPC is a high-performance communication protocol.
 import * as activities from "./activities";
+// activities → your real activity implementations (DB calls, APIs, etc.)
 
+// This wraps the worker lifecycle:
+// connect
+// start worker
+// clean up on shutdown
 async function run() {
   // Step 1: Establish a connection with Temporal server.
   //
   // Worker code uses `@temporalio/worker.NativeConnection`.
   // (But in your application code it's `@temporalio/client.Connection`.)
+
+  // The worker opens a persistent connection to Temporal Server
+  // This is not a database connection — it's to Temporal itself
   const connection = await NativeConnection.connect({
-    address: "localhost:7233",
+    address: "localhost:7233", // 7233 is Temporal’s default gRPC port
     // TLS and gRPC metadata configuration goes here.
   });
   try {
     // Step 2: Register Workflows and Activities with the Worker.
+    // Creating a worker
     const worker = await Worker.create({
       connection,
-      namespace: "default",
-      taskQueue: "hello-world",
-      // Workflows are registered using a path as they run in a separate JS context.
-      workflowsPath: require.resolve("./workflow"),
+      namespace: "default", // Logical isolation inside Temporal
+      taskQueue: "hello-world", // Any workflow/activity sent to this queue will be picked up by this worker
+      // Workflows are registered using a path as they run in a separate JS context. workflows run in a separate JS sandbox
+      workflowsPath: require.resolve("./workflow"), // tells temporal where the workflow lives
+      // Actual implementation for activities.
+      // functions that can: hit DB, call APIs, do side effects
       activities,
     });
 
@@ -36,6 +55,7 @@ async function run() {
   }
 }
 
+// Start everything
 run().catch((err) => {
   console.error(err);
   process.exit(1);
